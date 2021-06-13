a6f6a23ea8f551dbb880ce1ae9924d0eef4d0f69
boomshair <957904176@qq.com>
2021-06-13 13:21:52 +0800

    update

--- firstShow.java
+++ firstShow.java
@@ -3,5 +3,7 @@ public class firstShow {
     public static void main(String[] args) {
         String x = "java";
         System.out.println(x);
+        String text = "将git数据放入暂存区中";
+        System.out.println(text);
     }
 }
