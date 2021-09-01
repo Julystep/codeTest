e199859630c3c355d3375ab4994e2feafefbfdcd
boomshair <957904176@qq.com>
2021-09-01 22:23:05 +0800

    删除

--- firstShow.java
+++
@@ -1,9 +0,0 @@
-public class firstShow {
-
-    public static void main(String[] args) {
-        String x = "java";
-        System.out.println(x);
-        String text = "将git数据放入暂存区中";
-        System.out.println(text);
-    }
-}
